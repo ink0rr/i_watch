8269200a5040701478662c000d1c2080503a04f5
minxs33 <47031547+minxs33@users.noreply.github.com>
2022-06-28 23:41:43 +0700

    favicon and fixing minor error on header

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,31 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=esc($title)?></title>
     <link  rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.css')?>"/>
+    <link rel="icon" href="<?=base_url('assets/logo-16x16.png')?>" type="image">
     <script src="<?=base_url('assets/js/bootstrap.js')?>"></script>
     <script src="<?=base_url('assets/js/anime.min.js')?>"></script>
     <script src="<?=base_url('assets/js/jquery.js')?>"></script>
 </head>
 <body>
     <header>
+
+    <!-- static navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#B25068;">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="<?=base_url('assets/logo.png')?>" alt="IOO Watch" width="227" height="58">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerStatic" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div class="collapse navbar-collapse" id="navbarTogglerStatic">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item mx-2">
+                <li class="nav-item">
                     <a class="nav-link active" href="#">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Perlu Bantuan</a>
                 </li>
             </ul>
-            <ul class="navbar-nav mb-2 mb-lg-0 ">
-                <li class="nav-item mx-3">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
                     <a class="nav-link active" href="#">Daftar</a>
                 </li>
                 <li class="nav-item">
@@ -41,15 +44,16 @@
         </div>
     </nav>
 
+    <!-- sticky navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top sticky-navbar shadow-sm mb-5 bg-body rounded" style="transform:translateY(-76px);">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="<?=base_url('assets/logo_icon.png')?>" alt="IOO Watch">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerSticky" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div class="collapse navbar-collapse" id="navbarTogglerSticky">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Tentang Kami</a>

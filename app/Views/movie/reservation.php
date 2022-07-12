9dce46b4b834e25712cf616572b84de9cbf0a35f
minxs33 <47031547+minxs33@users.noreply.github.com>
2022-07-12 19:40:34 +0700

    Fixing script after merge

--- app/Views/movie/reservation.php
+++ app/Views/movie/reservation.php
@@ -60,6 +60,9 @@
             naming: {
                 rows: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
                 columns: ['1', '2', '3', '4', '', '5', '6', '7', '8', '', '9', '10', '11', '12'],
+                getId: function(character, row, column) {
+                    return row + column;
+                },
                 getLabel: function(character, row, column) {
                     return row + column;
                 },

f0c28e549720f18b0e8cbbc010f42f03404a5b18
minxs33 <47031547+minxs33@users.noreply.github.com>
2022-07-15 14:26:30 +0700

    custom password on users seed

--- app/Database/Seeds/Users.php
+++ app/Database/Seeds/Users.php
@@ -12,17 +12,17 @@ class Users extends Seeder
             [
                 'name' => 'Tomflynn Beltsazar',
                 'email' => 'test@gmail.com',
-                'password' => password_hash('test1234', PASSWORD_DEFAULT)
+                'password' => '$2y$10$ZALK9FxVY2f7ImByQs4G8ezkbbzJIm6KKA5KE4ACeloH7dESvaEZ.'
             ],
             [
                 'name' => 'Ari Candra',
                 'email' => 'test2@gmail.com',
-                'password' => password_hash('test1234', PASSWORD_DEFAULT)
+                'password' => '$2y$10$ZALK9FxVY2f7ImByQs4G8ezkbbzJIm6KKA5KE4ACeloH7dESvaEZ.'
             ],
             [
                 'name' => 'Joko Sulistyo',
                 'email' => 'test3@gmail.com',
-                'password' => password_hash('test1234', PASSWORD_DEFAULT)
+                'password' => '$2y$10$ZALK9FxVY2f7ImByQs4G8ezkbbzJIm6KKA5KE4ACeloH7dESvaEZ.'
             ]
         ]);
     }
